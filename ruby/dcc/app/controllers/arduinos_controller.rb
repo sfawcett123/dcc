00c14f92e0be4213fa0da49f3b35ca3257a4fef2
Steven Fawcett <steve.fawcett@capgemini.com>
2022-01-05 13:11:16 +0000

    Improved Arduino Listing

--- ruby/dcc/app/controllers/arduinos_controller.rb
+++ ruby/dcc/app/controllers/arduinos_controller.rb
@@ -1,32 +1,15 @@
 class ArduinosController < ApplicationController
   layout 'standard'
   
-  before_action :set_arduino, only: %i[ show edit update destroy ]
-
   # GET /arduinos or /arduinos.json
   def index
     Arduino.destroy
-    create()
-    @arduinos = Arduino.all
-  end
-
-  def create
     commandString = "arduino-cli board list --format json"
     devices = %x[ #{commandString} ]
     JSON.parse( devices).each do | device |
       @arduino = Arduino.new( device )
-      puts @arduino
     end
+    @arduinos = Arduino.boards
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_arduino
-      @arduino = Arduino.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def arduino_params
-      params.require(:arduino).permit(:name, :port)
-    end
 end
--- ruby/dcc/app/models/arduino.rb
+++ ruby/dcc/app/models/arduino.rb
@@ -1,29 +1,38 @@
 
 class Arduino
 
-    include ActiveModel::Model
+   include ActiveModel::Model
 
-    attr_accessor :name, :port, :matching_boards
+   @vendorId="0x2341"
+   @@_arduinos = []
 
-    @@_arduinos = []
-
-    def initialize(attributes = {})
-      attributes.each do |name, value|
-       send("#{name}=", value)
+   def initialize(attributes = {} , list=true )
+       attributes.each do |name, value|
+           if value.is_a? Hash
+             initialize(value , false )
+           elsif value.is_a? Array
+             value.each do |n|
+                 initialize(n , false )
+             end
+           else
+             self.class.send(:attr_accessor, name)
+             instance_variable_set( "@#{name}" , value)
+           end
       end
-      @@_arduinos << self
-    end
+      @@_arduinos << self if list
+   end
 
-    def persisted?
-      false
-    end
-    class << self
-      def all
-        return @@_arduinos
+   def persisted?
+     false
+   end
+
+   class << self
+      def boards
+        return @@_arduinos.select { |n| n.vid == @vendorId }
       end
       def destroy()
         @@_arduinos = []
       end
-    end
+   end
 
 end
