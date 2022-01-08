1ff8fbf04daf4e8aa084a1b0b8389321e2a8af34
Steven Fawcett <stevenfawcett@MacBook-Pro.local>
2022-01-08 19:24:50 +0000

    Save USB record

--- ruby/usbapi/app/models/usb.rb
+++ ruby/usbapi/app/models/usb.rb
@@ -1,20 +1,21 @@
 # frozen_string_literal: true
 
 class Usb < ApplicationRecord
-  attr_accessor :device
-
-  def initialize(device:)
-    self.device = device
-  end
+  attr_accessor :port
+  after_initialize :process_device
 
   private
 
-  def indexed_devices
-      @indexed_devices ||= usb.index_by(&:id)
+   def process_device
+       self.address = port["address"]     
+       self.label = port["label"]     
+   end
+
+  def labelled_devices
+    @labelled_devices ||= usb.index_by(&:id)
   end
 
   def fetch(id)
-      indexed_devices.fetch(id)
+    labelled_devices.fetch(id)
   end
-
 end
--- ruby/usbapi/app/workers/listusb_worker.rb
+++ ruby/usbapi/app/workers/listusb_worker.rb
@@ -11,12 +11,10 @@ class ListusbWorker
 
     JSON.parse(devices).each do |device|
       logger.info "Device Found #{device['port']['address']} "
-      Usb.new(device: device)
+      device = Usb.new(device)
+      device.save()
     end
 
-    already_scheduled = Sidekiq::ScheduledSet.new.any? { |job| job.klass == 'ListusbWorker' }
-    logger.info "Already Scheduled #{already_scheduled}"
-
-    ListusbWorker.perform_in(10.seconds) unless already_scheduled
+    ListusbWorker.perform_in(10.seconds)
   end
 end
