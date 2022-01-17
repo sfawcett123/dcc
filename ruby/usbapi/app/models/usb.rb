b95df9649b6d1c576bb2a6bdc405fb33f257a876
Steven Fawcett <steve.fawcett@capgemini.com>
2022-01-17 11:49:20 +0000

    CSS improvement

--- ruby/usbapi/app/models/usb.rb
+++ ruby/usbapi/app/models/usb.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Usb < ApplicationRecord
+  def connected? 
+    @serialnumber ? true : false
+  end
+
   class << self
     def connected
       all.select(&:serialnumber)
