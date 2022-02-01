566dea3068589d1d8b9cebb045806ab33483df10
Steven Fawcett <steve.fawcett@capgemini.com>
2022-02-01 22:43:45 +0000

    store database URL

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -6,3 +6,16 @@ data "aws_secretsmanager_secret" "db" {
 data "aws_secretsmanager_secret_version" "db" {
   secret_id = data.aws_secretsmanager_secret.db.id
 }
+
+locals {
+ secrets = {
+  username = jsondecode(data.aws_secretsmanager_secret_version.db.secret_string)["username"]
+  password = jsondecode(data.aws_secretsmanager_secret_version.db.secret_string)["password"]
+  database = aws_db_instance.database.endpoint 
+ }
+}
+
+resource "aws_secretsmanager_secret_version" "save" {
+  secret_id = data.aws_secretsmanager_secret.db.id
+  secret_string = jsonencode( local.secrets )
+}
